fa3519a1f6c155cb53b0eb5422348d35e0424fc7
Antoni Gimenez <antoni.gimenez@outlook.es>
2023-09-27 12:59:45 +0200

    Afegim import desgravat en esponsor Refactoring vistes, cada vista en un xml

--- modul_xativa/__manifest__.py
+++ modul_xativa/__manifest__.py
@@ -8,5 +8,16 @@
     'website': "http://www.clubhandbolxativa.com",
     'category': 'Uncategorized',
     'version': '11.0.1.0.0',
-    'data': ['security/ir.model.access.csv','views/views.xml'],
+    'data': ['security/ir.model.access.csv',
+             'views/views_cobroPago.xml',
+             'views/views_directiva.xml',
+             'views/views_empresa.xml',
+             'views/views_equip.xml',
+             'views/views_esponsor.xml',
+             'views/views_factures.xml',
+             'views/views_nomina.xml',
+             'views/views_persona.xml',
+             'views/views_temporada.xml',
+             'views/views.xml'
+             ],
 }
--- modul_xativa/models/xativa_esponsor.py
+++ modul_xativa/models/xativa_esponsor.py
@@ -14,6 +14,7 @@ class XativaEsponsor(models.Model):
     factura = fields.Binary('Factura', required=False)
     justificant = fields.Binary('Justificant')
     cobrat = fields.Boolean('Cobrat')
+    importDesgravat = fields.Float('Import desgravat')
 
     equips_ids = fields.Many2many('xativa.equip', 'equip_esponsor_rel', 'esponsor_id', 'equip_id', string='Equips esponsoritzats')
     temporada_id = fields.Many2one('xativa.temporada', string='Temporada')
