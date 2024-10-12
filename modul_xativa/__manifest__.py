0a2e794952355aaa3e8fa41ef6d4ea96855dc099
Antoni Gimenez <antoni.gimenez@outlook.es>
2024-10-12 11:36:37 +0200

    Actualtizem el modul a la V17 d'odoo

--- modul_xativa/__manifest__.py
+++ modul_xativa/__manifest__.py
@@ -7,7 +7,7 @@
     'license': "AGPL-3",
     'website': "http://www.clubhandbolxativa.com",
     'category': 'Uncategorized',
-    'version': '11.0.1.0.0',
+    'version': '17.0.1.0.0',
     'data': ['security/ir.model.access.csv',
              'views/views_actesJunta.xml',
             #  'views/views_camisa.xml',
